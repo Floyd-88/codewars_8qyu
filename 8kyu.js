fc9c74e3e9f0ee421092a51693598489d8e81a6a
Floyd-88 <ilasaz1988@gmail.com>
2022-01-27 22:24:41 +0300

    filter array

--- 8kyu.js
+++ 8kyu.js
@@ -24,8 +24,16 @@ function numberToString(num) {
 
 /*Task
 Given an integral number, determine if it's a square number:*/
-
 var isSquare = function(n){
   return Number.isInteger(Math.sqrt(n));
   }; // fix me
 console.log(isSquare(16))
+
+
+/*Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...*/
+let friends = ["Ryan", "Kieran", "Mark"];
+function friend(friends){
+  let lengthCheck = friends.filter( item => item.length === 4);
+  return lengthCheck;
+}
