c9f28cd758cb7287679eb3a65beb940cb62ca245
Floyd-88 <ilasaz1988@gmail.com>
2022-01-28 23:13:01 +0300

    Growth of a Population

--- 8kyu.js
+++ 8kyu.js
@@ -69,3 +69,18 @@ function invert(array) {
 }
 let array = [1,-2,3,0,5];
 console.log(invert(array));
+
+
+
+/*In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?*/
+function nbYear(p0, percent, aug, p) {
+	let percentPeople = 0;
+	let i = 0;
+	while (p0 < p) {
+	percentPeople = p0 / 100 * percent;
+	p0 += aug + percentPeople;
+	i = i+1;
+  }
+  return i;
+}
+console.log(nbYear(1500000, 0.25, 1000, 2000000))
