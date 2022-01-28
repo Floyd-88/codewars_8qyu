ea73d541f175a686eeec16609872a7e52e13c76c
Floyd-88 <ilasaz1988@gmail.com>
2022-01-28 21:14:35 +0300

    bool

--- 8kyu.js
+++ 8kyu.js
@@ -52,3 +52,13 @@ return (numbers.length != 0) ? sum.pop() : 0;
 }
 
 console.log(squareSum(numbers));
+
+
+
+/*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
+function boolToWord( bool ){
+return (bool === true) ? "Yes" : "No";
+}
+console.log(boolToWord( false ))
+
+
