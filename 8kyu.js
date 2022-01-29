b8febbbce4db5491cd5ba4bd9f1dc534d34e3769
Floyd-88 <ilasaz1988@gmail.com>
2022-01-29 18:37:34 +0300

    A Needle in the Haystack

--- 8kyu.js
+++ 8kyu.js
@@ -119,5 +119,17 @@ function disemvowel(str) {
 		return strArrNew.join("")
 }
 let str = "hjkajliOyj gfdg aaad"
-
 console.log(disemvowel(str))
+
+
+/*Can you find the needle in the haystack?
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+After your function finds the needle it should return a message (as a string) that says:
+"found the needle at position " plus the index it found the needle, so:*/
+
+function findNeedle(haystack) {
+  let result = haystack.indexOf("needle");
+  if(result != -1) {return "found the needle at position " + result}
+}
+var haystack_3 = [1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54];
+console.log(findNeedle(haystack_3))
